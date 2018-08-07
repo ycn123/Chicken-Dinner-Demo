2d28137d0dd9066bcaa2cf0c7fe39930efe3de5c
ycn123 <32393132+ycn123@users.noreply.github.com>
2018-08-07 19:11:35 +0800

    重新制作了地形

---
+++ Chicken Dinner Demo 01/Assets/Scripts/PlayerTreatment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTreatment : MonoBehaviour {
+    public int Hp;
+	// Use this for initialization
+	void Start () {
+        if (Hp > 100) {
+            Hp = 100; 
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
