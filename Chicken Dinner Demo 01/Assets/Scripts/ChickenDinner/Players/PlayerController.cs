6482fd16020c127c4f4feca90ff3e7f2659fd796
ycn123 <32393132+ycn123@users.noreply.github.com>
2018-08-19 15:02:41 +0800

    人物移动

---
+++ Chicken Dinner Demo 01/Assets/Scripts/ChickenDinner/Players/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Animator animator;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+    }
+    void Move()
+    {
+        var h = Input.GetAxis("Horizontal");
+        Debug.Log(h);
+        var v = Input.GetAxis("Vertical");
+        animator.SetFloat("speedX", h*1.76f);
+        animator.SetFloat("speedZ", v*2.71f);
+    }
+}
